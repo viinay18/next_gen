97fe9659d857414173c60e008597d38f965150c7
viinay18 <vinaykasani98@gmail.com>
2024-11-27 09:46:09 +0530

    my commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>reactjs project is successfully eecuted</h1>
+      <marquee>Welcome to GNIT College</marquee>
+    </div>
+  );
+}
+
+export default App;
